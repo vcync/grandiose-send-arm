4a00c33e23af517bf8541a1bbaf77564ef1f601c
Ralf S. Engelschall <rse@engelschall.com>
2021-03-24 13:19:37 +0100

    remove bad code

--- src/grandiose_util.cc
+++ src/grandiose_util.cc
@@ -66,48 +66,6 @@ char* custom_itoa(int num, char* str, int base)
   return str;
 }
 
-// Implementation of itoa()
-char* custom_itoa(int num, char* str, int base)
-{
-  int i = 0;
-  bool isNegative = false;
-
-  /* Handle 0 explicitely, otherwise empty string is printed for 0 */
-  if (num == 0)
-  {
-    str[i++] = '0';
-    str[i] = '\0';
-    return str;
-  }
-
-  // In standard itoa(), negative numbers are handled only with
-  // base 10. Otherwise numbers are considered unsigned.
-  if (num < 0 && base == 10)
-  {
-    isNegative = true;
-    num = -num;
-  }
-
-  // Process individual digits
-  while (num != 0)
-  {
-    int rem = num % base;
-    str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
-    num = num/base;
-  }
-
-  // If number is negative, append '-'
-  if (isNegative)
-    str[i++] = '-';
-
-  str[i] = '\0'; // Append string terminator
-
-  // Reverse the string
-  std::reverse(std::string(str).begin(), std::string(str).end());
-
-  return str;
-}
-
 napi_status checkStatus(napi_env env, napi_status status,
   const char* file, uint32_t line) {
 
